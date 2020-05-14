1e08a9a714726ed7ef28a0c14fa63e3e5b16f11b
Sergey Mironov <53237933+SergeiMironov@users.noreply.github.com>
2020-05-14 10:09:30 +0400

    Загрузка задания

---
+++ Project - #27/Make_module.cs
@@ -0,0 +1,20 @@
+    class Parent
+    {
+        public int x, y;        
+        public Parent()
+        {             
+            x = 10;             
+            y = 20;
+        }
+    }
+    class child : Parent
+    {
+        public int z;
+        public int y;         
+        public child()
+        {             
+            y = 30;             
+            z = 50;         
+        }
+    }
+
